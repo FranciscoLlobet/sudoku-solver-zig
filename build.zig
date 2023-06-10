69f83a337b5f47ef455b97fb7bf95dbf3d49c7ef
Francisco Llobet <llf7rt@bosch.com>
2023-06-10 15:48:52 +0200

    move to zig 0.11.0(dev)

--- build.zig
+++ build.zig
@@ -1,33 +1,26 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.Build) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const mode = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
 
-    const lib = b.addStaticLibrary("sudoku-solver-zig", "src/sudoku-solver.zig");
-    lib.setBuildMode(mode);
-    lib.install();
+    const lib = b.addStaticLibrary(.{ .name = "sudoku-solver-zig", .root_source_file = .{ .path = "src/sudoku-solver.zig" }, .optimize = mode, .target = target });
+    b.installArtifact(lib);
 
-    const exe = b.addExecutable("main", "src/main.zig");
-    exe.setBuildMode(mode);
-    exe.install();
+    const exe = b.addExecutable(.{ .name = "main", .root_source_file = .{ .path = "src/main.zig" }, .optimize = mode, .target = target });
+    b.installArtifact(exe);
 
-    const unit_tests = b.addTest("src/sudoku-solver.zig");
-    unit_tests.setBuildMode(mode);
-
-    const data_tests = b.addTest("test_data.zig");
-    data_tests.setBuildMode(mode);
+    const unit_tests = b.addTest(.{ .name = "unit_test", .root_source_file = .{ .path = "src/sudoku-solver.zig" }, .optimize = mode, .target = target });
+    const data_tests = b.addTest(.{ .name = "data_test", .root_source_file = .{ .path = "test_data.zig" }, .optimize = mode, .target = target });
 
     const unit_test_step = b.step("test", "Run sudoku-solver library tests");
     unit_test_step.dependOn(&unit_tests.step);
     unit_test_step.dependOn(&data_tests.step);
 
-    const run_exe = exe.run();
+    const run_exe = b.addRunArtifact(exe);
     run_exe.step.dependOn(b.getInstallStep());
-    //if (b.args) |args| {
-    //    run_exe.addArg(args);
-    //}
 
     const run_step = b.step("run", "Run sudoku-solver");
     run_step.dependOn(&run_exe.step);
